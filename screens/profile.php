fdd6a8eebf633c53f3706d25ef63c3e39df1ccf7
D. Klaasen <danielle.klaasen@hva.nl>
2017-11-08 10:10:24 +0100

    cleaning up directories
    removing php, replaced by html

--- screens/profile.php
+++
@@ -1,6 +0,0 @@
-<div class="wdw container profile-wdw">
-    <div class="wrapper">
-        <div class="top-space"></div>
-        <h2>Welcome Danielle</h2>
-    </div>
-</div>
--- screens/search.php
+++
@@ -1,4 +0,0 @@
-<div class="search-wdw">
-    <svg class="lnr lnr-cross link close-search"><use xlink:href="#lnr-cross"></use></svg>
-    <input id="search-input" class="big-input" type="text" name="search" placeholder="Search..">
-</div>
